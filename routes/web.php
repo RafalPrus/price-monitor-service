f080b5af0b8c43b822ab6e0a3f26b5d6962c65ad
RafalPrus <rafalprues@gmail.com>
2023-12-23 16:35:19 +0100

    Add laravel sail

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    \App\Jobs\ProcessProductScan::dispatch();
+    // \App\Jobs\ProcessProductScan::dispatch();
     return view('welcome');
 });
