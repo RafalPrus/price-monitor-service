e83ecd4e576e5bf9d4ac1f3b3a4435fa96250d80
RafalPrus <123735569+RafalPrus@users.noreply.github.com>
2024-01-02 12:24:37 +0100

    Add new OfferController test, add Offer Factory

---
+++ database/factories/OfferFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Offer>
+ */
+class OfferFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory()->create(),
+            'name' => fake()->name(),
+            'url' => fake()->url(),
+            'is_active' => fake()->numberBetween(0, 1),
+        ];
+    }
+}
